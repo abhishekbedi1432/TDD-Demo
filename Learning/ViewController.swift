005ddb9651e8a122012eead729466b3b1c03df9e
Abhishek Bedi <abhishek.bedi@igt.in>
2017-11-17 16:47:42 +0530

    Initial Commit

---
+++ Learning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning
+//
+//  Created by Abhishek Bedi on 17/11/2017.
+//  Copyright © 2017 Abhishek Bedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
